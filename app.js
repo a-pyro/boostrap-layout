d27d8d62871b5fc57eff4dc632826b19ea507924
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2020-11-03 18:13:21 +0100

    home, search, library section done

--- app.js
+++ app.js
@@ -26,47 +26,46 @@ document.addEventListener('DOMContentLoaded', () => {
 
   //   create the basic bootstrap grid
   //   2 row 6 col
-  const firstRow = document.createElement('div');
-  firstRow.classList.add('row', 'first-row', 'justify-content-start');
-  const secondRow = document.createElement('div');
-  secondRow.classList.add('row', 'second-row', 'justify-content-start');
-  container.append(firstRow, secondRow);
+  // const firstRow = document.createElement('div');
+  // firstRow.classList.add('row', 'first-row', 'justify-content-start');
+  // const secondRow = document.createElement('div');
+  // secondRow.classList.add('row', 'second-row', 'justify-content-start');
+  // container.append(firstRow, secondRow);
 
   //   create col and append to rows
-  for (let i = 0; i < 6; i++) {
-    const colsFirstRow = document.createElement('div');
-    const colsSecondRow = document.createElement('div');
-    colsFirstRow.classList.add(
-      'col-12',
-      'col-sm-6',
-      'col-md-4',
-      'col-lg-2',
-      'text-center',
-      'mb-3',
-      'col-js'
-    );
-    colsSecondRow.classList.add(
-      'col-12',
-      'col-sm-6',
-      'col-md-4',
-      'col-lg-2',
-      'text-center',
-      'mb-3',
-      'col-js'
-    );
-    firstRow.append(colsFirstRow);
-    secondRow.append(colsSecondRow);
-  }
+  // for (let i = 0; i < 6; i++) {
+  //   const colsFirstRow = document.createElement('div');
+  //   const colsSecondRow = document.createElement('div');
+  //   colsFirstRow.classList.add(
+  //     'col-12',
+  //     'col-sm-6',
+  //     'col-md-4',
+  //     'col-lg-2',
+  //     'text-center',
+  //     'mb-3',
+  //     'col-js'
+  //   );
+  //   colsSecondRow.classList.add(
+  //     'col-12',
+  //     'col-sm-6',
+  //     'col-md-4',
+  //     'col-lg-2',
+  //     'text-center',
+  //     'mb-3',
+  //     'col-js'
+  //   );
+  //   firstRow.append(colsFirstRow);
+  //   secondRow.append(colsSecondRow);
+  // }
 
   //   load the images
-  const columns = document.querySelectorAll('.col-js');
-  for (let i = 0; i < columns.length; i++) {
-    columns[i].appendChild(createRandomImgArray(columns.length, 400, 200)[i]);
-  }
+  // const columns = document.querySelectorAll('.col-js');
+  // for (let i = 0; i < columns.length; i++) {
+  //   columns[i].appendChild(createRandomImgArray(columns.length, 400, 200)[i]);
+  // }
 
   //   changin bg color to the navbar on scroll
   const navBar = document.querySelector('#navBar');
-
   window.addEventListener('scroll', (e) => {
     const yPos = window.scrollY;
     if (yPos >= 25) {
@@ -78,49 +77,4 @@ document.addEventListener('DOMContentLoaded', () => {
       navBar.classList.remove('navbar-custom');
     }
   });
-
-  const home = document.querySelector('.navbar-brand.mr-auto');
-  console.log(home);
-  home.addEventListener('click', () => {
-    console.log('i work');
-    const elementToshow = document.querySelector('#provaRow');
-    elementToshow.classList.toggle('invisible');
-    const myPage = document.querySelector('#myPage');
-    myPage.classList.toggle('opaco');
-  });
-
-  //   const yPos = window.scrollY;
-  //   if (yPos >= 25) {
-  //     navBar.classList.remove('bg-transparent');
-  //     navBar.classList.add('navbar-custom');
-
-  //     navBar.style.transform = 'translateY(-70px)';
-
-  //     setTimeout(function () {
-  //       navBar.style.transform = 'translateY(0)';
-  //     }, 200);
-  //   }
-  //   if (yPos === 0) {
-  //     navBar.classList.add('bg-transparent');
-  //     navBar.classList.remove('navbar-custom');
-  //   }
-  // });
 });
-
-// let scrolled = false;
-
-// window.onscroll = function () {
-//   if (window.pageYOffset > 30) {
-//     navBar.classList.remove('bg-transparent');
-//     if (!scrolled) {
-//       navBar.style.transform = 'translateY(-70px)';
-//     }
-//     setTimeout(function () {
-//       navBar.style.transform = 'translateY(0)';
-//       scrolled = true;
-//     }, 200);
-//   } else {
-//     navBar.classList.add('bg-dark');
-//     scrolled = false;
-//   }
-// };
