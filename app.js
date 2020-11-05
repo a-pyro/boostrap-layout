2d71042f279da2c0eb08b0fc0a2d06836fd3604f
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2020-11-05 13:24:03 +0100

    finished side menu, started main showcase

--- app.js
+++ app.js
@@ -1,80 +1,80 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('dom loaded');
-  // smooth scroll
-  const scroll = new SmoothScroll('a[href*="#"]');
-  const container = document.querySelector('.container-fluid');
+    console.log('dom loaded');
+    // smooth scroll
+    const scroll = new SmoothScroll('a[href*="#"]');
+    const container = document.querySelector('.container-fluid');
 
-  // random img array function
-  const createRandomImgArray = (numberOfImages, width, height) => {
-    const arrayOfImg = [];
-    for (let i = 0; i < numberOfImages; i++) {
-      const imgLink = `https://picsum.photos/${String(width)}/${String(
-        height
-      )}?random=${Math.ceil(Math.random() * 100)}`;
-      const newAnchor = document.createElement('a');
-      newAnchor.setAttribute('target', '_blank');
-      newAnchor.setAttribute('title', 'click to open');
-      newAnchor.setAttribute('href', imgLink);
-      const newImg = document.createElement('img');
-      newImg.setAttribute('src', imgLink);
-      newImg.classList.add('img-fluid');
-      newAnchor.appendChild(newImg);
-      arrayOfImg.push(newAnchor);
-    }
-    return arrayOfImg;
-  };
+    // random img array function
+    const createRandomImgArray = (numberOfImages, width, height) => {
+        const arrayOfImg = [];
+        for (let i = 0; i < numberOfImages; i++) {
+            const imgLink = `https://picsum.photos/${String(width)}/${String(
+                height
+            )}?random=${Math.ceil(Math.random() * 100)}`;
+            const newAnchor = document.createElement('a');
+            newAnchor.setAttribute('target', '_blank');
+            newAnchor.setAttribute('title', 'click to open');
+            newAnchor.setAttribute('href', imgLink);
+            const newImg = document.createElement('img');
+            newImg.setAttribute('src', imgLink);
+            newImg.classList.add('img-fluid');
+            newAnchor.appendChild(newImg);
+            arrayOfImg.push(newAnchor);
+        }
+        return arrayOfImg;
+    };
 
-  //   create the basic bootstrap grid
-  //   2 row 6 col
-  // const firstRow = document.createElement('div');
-  // firstRow.classList.add('row', 'first-row', 'justify-content-start');
-  // const secondRow = document.createElement('div');
-  // secondRow.classList.add('row', 'second-row', 'justify-content-start');
-  // container.append(firstRow, secondRow);
+    //   // create the basic bootstrap grid
+    //   // 2 row 6 col
+    // const firstRow = document.createElement('div');
+    // firstRow.classList.add('row', 'first-row', 'justify-content-start');
+    // const secondRow = document.createElement('div');
+    // secondRow.classList.add('row', 'second-row', 'justify-content-start');
+    // container.append(firstRow, secondRow);
 
-  //   create col and append to rows
-  // for (let i = 0; i < 6; i++) {
-  //   const colsFirstRow = document.createElement('div');
-  //   const colsSecondRow = document.createElement('div');
-  //   colsFirstRow.classList.add(
-  //     'col-12',
-  //     'col-sm-6',
-  //     'col-md-4',
-  //     'col-lg-2',
-  //     'text-center',
-  //     'mb-3',
-  //     'col-js'
-  //   );
-  //   colsSecondRow.classList.add(
-  //     'col-12',
-  //     'col-sm-6',
-  //     'col-md-4',
-  //     'col-lg-2',
-  //     'text-center',
-  //     'mb-3',
-  //     'col-js'
-  //   );
-  //   firstRow.append(colsFirstRow);
-  //   secondRow.append(colsSecondRow);
-  // }
+    //   // create col and append to rows
+    // for (let i = 0; i < 6; i++) {
+    //   const colsFirstRow = document.createElement('div');
+    //   const colsSecondRow = document.createElement('div');
+    //   colsFirstRow.classList.add(
+    //     'col-12',
+    //     'col-sm-6',
+    //     'col-md-4',
+    //     'col-lg-2',
+    //     'text-center',
+    //     'mb-3',
+    //     'col-js'
+    //   );
+    //   colsSecondRow.classList.add(
+    //     'col-12',
+    //     'col-sm-6',
+    //     'col-md-4',
+    //     'col-lg-2',
+    //     'text-center',
+    //     'mb-3',
+    //     'col-js'
+    //   );
+    //   firstRow.append(colsFirstRow);
+    //   secondRow.append(colsSecondRow);
+    // }
 
-  //   load the images
-  // const columns = document.querySelectorAll('.col-js');
-  // for (let i = 0; i < columns.length; i++) {
-  //   columns[i].appendChild(createRandomImgArray(columns.length, 400, 200)[i]);
-  // }
+    //   // load the images
+    // const columns = document.querySelectorAll('.col-js');
+    // for (let i = 0; i < columns.length; i++) {
+    //   columns[i].appendChild(createRandomImgArray(columns.length, 400, 200)[i]);
+    // }
 
-  //   changin bg color to the navbar on scroll
-  const navBar = document.querySelector('#navBar');
-  window.addEventListener('scroll', (e) => {
-    const yPos = window.scrollY;
-    if (yPos >= 25) {
-      navBar.classList.remove('bg-transparent');
-      navBar.classList.add('navbar-custom');
-    }
-    if (yPos === 0) {
-      navBar.classList.add('bg-transparent');
-      navBar.classList.remove('navbar-custom');
-    }
-  });
+    //   changin bg color to the navbar on scroll
+    const navBar = document.querySelector('#navBar');
+    window.addEventListener('scroll', (e) => {
+        const yPos = window.scrollY;
+        if (yPos >= 25) {
+            navBar.classList.remove('bg-transparent');
+            navBar.classList.add('navbar-custom');
+        }
+        if (yPos === 0) {
+            navBar.classList.add('bg-transparent');
+            navBar.classList.remove('navbar-custom');
+        }
+    });
 });
